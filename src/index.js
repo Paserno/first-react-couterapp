f3ba68365a64e03b506f119abbc78aef60b74e10
Paserno <paserno.x@gmail.com>
2021-11-26 12:30:14 -0300

    Primer Componente

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+
+import './index.css';
 
 
-
-
-const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot =  document.querySelector('#root');
 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render( <PrimeraApp /> ,divRoot);
+
