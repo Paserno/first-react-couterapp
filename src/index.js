f9b243fba3601111039d777432b637c81eed4b97
Paserno <paserno.x@gmail.com>
2021-11-28 22:45:05 -0300

    Tarea: Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
 
 
 
-const divRoot =  document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp saludo="Hola mundo"/> ,divRoot);
+const divRoot =  document.querySelector('#root');
+//<PrimeraApp saludo="Hola mundo"/> 
+ReactDOM.render( <CounterApp value={123}/>,divRoot);
 
