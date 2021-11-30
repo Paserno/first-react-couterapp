64854aff48ce9c0c8f08ce01ec2c41deb3bf6dcc
Paserno <paserno.x@gmail.com>
2021-11-30 17:25:37 -0300

    Enzyme - Testing unit

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import {createSerializer} from 'enzyme-to-json';
+// import '@testing-library/jest-dom/extend-expect';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer( createSerializer({mode: 'deep'}) );
 
 
-
-import '@testing-library/jest-dom/extend-expect';
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import {shallow} from 'enzyme';
 import PrimeraApp from "../PrimeraApp";
 
 
 describe('Prueba en <PrimeraApp />', () => {
     
-    test('debe de mostrar el mensaje "Hola, soy Paserno" ', () => {
+    // test('debe de mostrar el mensaje "Hola, soy Paserno" ', () => {
         
+    //     const saludo = 'Hola, soy Paserno';
+
+    //     const {getByText} = render(<PrimeraApp saludo={saludo}/>);
+
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
+    
+    test('Debe de mostrar <PrimeraApp /> correctamente', () => {
+
         const saludo = 'Hola, soy Paserno';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>);
 
-        const {getByText} = render(<PrimeraApp saludo={saludo}/>);
-
-        expect(getByText(saludo)).toBeInTheDocument();
+        expect(wrapper).toMatchSnapshot();
     });
     
     
-})
+});
